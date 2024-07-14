232c5a67c7f36420ef94159537dbdc2b9ad1b773
tamim <tamimyousefi023@gmail.com>
2024-07-14 23:07:09 +0330

    creating react app and ci file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
-function App() {
-  return <div className="App"></div>;
-}
 
-export default App;
+import	React	from	'react';
+function	App()	{
+		return	(
+				<div>
+						<h1>Hello,	world!</h1>
+				</div>
+		);
+}
+export	default	App;
---
+++ src/App.test.js
@@ -0,0 +1,7 @@
+import	{	render,	screen	}	from	'@testing-library/react';
+import	App	from	'./App';
+test('renders	Hello,	world!	text',	()	=>	{
+		render(<App	/>);
+		const	linkElement	=	screen.getByText(/Hello,	world!/i);
+		expect(linkElement).toBeInTheDocument();
+});
