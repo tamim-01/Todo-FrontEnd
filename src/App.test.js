ab666645c648dcff92aacfeab02d9f50382daeee
tamim <tamimyousefi023@gmail.com>
2024-07-17 21:41:25 +0330

    cleaning repository

--- src/App.test.js
+++
@@ -1,7 +0,0 @@
-import	{	render,	screen	}	from	'@testing-library/react';
-import	App	from	'./App';
-test('renders	Hello,	world!	text',	()	=>	{
-		render(<App	/>);
-		const	linkElement	=	screen.getByText(/Hello,	world!/i);
-		expect(linkElement).toBeInTheDocument();
-});
