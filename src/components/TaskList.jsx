373c15a7745ba28038fbc1d4b4e90cd76357f0eb
sepehrsharafi <sepehr.sharafi123@gmail.com>
2024-08-13 21:31:14 +0330

    Created Task List

---
+++ src/components/TaskList.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import TaskItem from "./TaskItem";
+
+const TaskList = ({ tasks, onDeleteTask, onEditTask }) => {
+  return (
+    <div className="w-3/4 h-min p-6 m-6 border-2 rounded-2xl">
+      <div className="flex justify-between items-center mb-6">
+        <h2 className="text-2xl font-bold">Today</h2>
+      </div>
+      <div className="space-y-4">
+        {tasks.map((task) => (
+          <TaskItem
+            key={task.id}
+            task={task}
+            onDelete={onDeleteTask}
+            onEdit={onEditTask}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TaskList;
