f8fccc38aab0bec245e104506fba8bbeea11a951
sepehrsharafi <sepehr.sharafi123@gmail.com>
2024-08-13 22:20:43 +0330

    Added functionality to the Edit button and modified styling

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,29 +1,31 @@
 import React from "react";
 import { Trash2, PenSquare } from "lucide-react";
 
-const TaskItem = ({ task, onDelete, onEdit }) => {
+const TaskItem = ({ task, onDelete, onEdit, onToggleCompletion }) => {
   return (
     <div className="bg-white p-4 rounded-lg shadow flex items-center justify-between">
       <div>
-        <h3 className="font-semibold">{task.title}</h3>
-        <p className="text-sm text-gray-500">{task.description}</p>
+        <h3 className="font-medium text-xl mb-1">{task.title}</h3>
+        <p className="text-lg text-gray-500">{task.description}</p>
       </div>
-      <div className="flex items-center space-x-2">
+      <div className="flex items-center gap-2">
         <input
           type="checkbox"
-          className="form-checkbox h-5 w-5 text-blue-600"
+          checked={task.is_completed}
+          onChange={() => onToggleCompletion(task.id)} // Call the prop function
+          className="my-auto w-5 h-5 rounded-3xl"
         />
         <button
-          className="text-red-500 hover:text-red-700"
-          onClick={() => onDelete(task.id)}
-        >
-          <Trash2 size={18} />
-        </button>
-        <button
-          className="text-blue-500 hover:text-blue-700"
+          className="text-blue-500 hover:text-blue-700 "
           onClick={() => onEdit(task.id)}
         >
-          <PenSquare size={18} />
+          <PenSquare size={24} />
+        </button>{" "}
+        <button
+          className="text-red-500 hover:text-red-700 -ml-0.5"
+          onClick={() => onDelete(task.id)}
+        >
+          <Trash2 size={24} />
         </button>
       </div>
     </div>
