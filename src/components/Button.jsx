ffa7c636953ee780f7cdc4ffe2ca8f8148c3f682
tamim <tamimyousefi023@gmail.com>
2024-08-13 19:35:59 +0330

    deleting files

--- src/components/Button.jsx
+++
@@ -1,5 +0,0 @@
-import { type } from "@testing-library/user-event/dist/type"
-import React from "react"
-export function Button ({style , text , onClickHandler , type}){
-return <button className={style} onClick={onClickHandler} type={type}> {text} </button>
-};
--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default module = {
-  content: ["./src/**/*.{html,js,jsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
