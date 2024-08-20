3f0fb388d3b97c115cbf01140a31bf78e6a23e30
sepehrsharafi <sepehr.sharafi123@gmail.com>
2024-08-20 16:59:23 +0330

    Added the routes required by use nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import TaskTracker from "./pages/Add-TaskPage.jsx";
+import Addtask from "./pages/Add-TaskPage.jsx";
 import TaskManager from "./pages/TaskManager.jsx";
 import { SigninPage } from "./pages/signin.jsx";
 import { SignupPage } from "./pages/signup.jsx";
+import Edittask from "./pages/Edit-TaskPage.jsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <TaskManager />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<SigninPage />} />
+        <Route path="/taskmanager" element={<TaskManager />} />
+        <Route path="/Addtaskpage" element={<Addtask />} />
+        <Route path="/Edittask" element={<Edittask />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
