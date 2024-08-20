2ac0104a6b9b58b462bdb6d036636eb8fd7a34fc
sepehrsharafi <sepehr.sharafi123@gmail.com>
2024-08-20 17:16:29 +0330

    Added functionality to the cancel button

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,19 @@
+import { useNavigate } from "react-router-dom";
+
 export default function Header() {
+  const navigate = useNavigate();
   return (
     <header className="flex justify-between items-center pt-4 pb-4 px-12 text-lg  mb-32 border-b-2">
       <div className="flex flex-row items-center">
         <img src="./logo.png" className="h-12" />
         <p className="font-bold text-xl ml-2">Task Manager</p>
       </div>
-      <button className="bg-red-500 text-white text-base p-2 px-5 rounded-lg">
+      <button
+        className="bg-red-500 text-white text-base p-2 px-5 rounded-lg"
+        onClick={() => {
+          navigate("/taskmanager");
+        }}
+      >
         Cancel
       </button>
     </header>
