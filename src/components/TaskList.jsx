faff446162756b622a7bb98e5aadba6a00825dde
sepehrsharafi <sepehr.sharafi123@gmail.com>
2024-08-22 19:48:43 +0330

    Cleaned code and made it responsive

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -26,14 +26,14 @@ const TaskList = ({
   });
 
   return (
-    <div className="w-3/4 h-min m-6">
+    <div className="w-full h-min p-6">
       {/* Tasks Due Section */}
       <div
         ref={tasksDueRef} // Attach the ref to the tasks due section
         className="border-2 border-gray-200 p-6 rounded-2xl mb-6"
       >
         <div className="flex justify-between items-center mb-4">
-          <h2 className="text-2xl font-bold">Tasks Due</h2>
+          <h2 className="text-2xl font-bold">Upcoming</h2>
         </div>
         <div className="space-y-4">
           {tasksDueTasks.map((task) => (
@@ -92,36 +92,10 @@ const TaskList = ({
               onOpenModal={onOpenModal} // Pass the openModal function to TaskItem
             />
           ))}
-          {completedTasks.length === 0 && <p>No past due tasks available.</p>}{" "}
+          {completedTasks.length === 0 && <p>No Completed tasks available.</p>}{" "}
           {/* Display a message if there are no past due tasks */}
         </div>
       </div>
-      {/* {completedTasks.length > 0 && (
-        <div
-          ref={completedTasksRef} // Attach the ref to the completed tasks section
-          className="border-2 border-gray-200 p-6 rounded-2xl"
-        >
-          <div className="flex justify-between items-center mb-4">
-            <h2 className="text-2xl font-bold">Completed Tasks</h2>
-          </div>
-          <div className="space-y-4">
-            {completedTasks.map((task) => (
-              <TaskItem
-                key={task.id} // Use task.id as the unique key
-                task={task} // Pass the task object to TaskItem
-                onDelete={onDeleteTask} // Pass the deleteTask function to TaskItem
-                onEdit={onEditTask} // Pass the editTask function to TaskItem
-                onToggleCompletion={onToggleTaskCompletion} // Pass the toggleTaskCompletion function to TaskItem
-                onOpenModal={onOpenModal} // Pass the openModal function to TaskItem
-              />
-            ))}
-          </div>
-        </div>
-      )}
-      {completedTasks.length === 0 && (
-        <p className="text-xl ml-7">No completed tasks available.</p>
-      )}{" "}
-      Display a message if there are no completed tasks */}
     </div>
   );
 };
